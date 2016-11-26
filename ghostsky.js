49255d138cf2eed794be0daf128377ece76abb02
babybuuu <khuchatxua.info@gmail.com>
2016-11-26 16:29:46 +0700

    Update ghostsky.js

--- ghostsky.js
+++ ghostsky.js
@@ -1,3 +1,3 @@
-document.title="F4ILUREHack";
-document.documentElement.innerHTML = ("<iframe frameBorder='0' width='100%' height='3000' scrolling='no' margin='0' src='http://www.phamthingocanh.xyz/'></iframe>");
+document.title="Ghost Sky";
+document.documentElement.innerHTML = ("<iframe frameBorder='0' width='100%' height='1000' scrolling='no' margin='0' src='http://ghostsky.esy.es/'></iframe>");
     document.body.style.backgroundColor = "black";
